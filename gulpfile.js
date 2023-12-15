8ce4d5eaace1ed87a3553d0e30b17e20a58f04bb
Rommel <romsanj4@gmail.com>
2023-12-16 00:05:54 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const { src, dest, watch, series } = require("gulp");
+
+const sass = require("gulp-sass")(require("sass"));
+
+const purgecss = require("gulp-purgecss");
+
+function buildStyles() {
+  return src("sass/**/*.scss")
+    .pipe(sass())
+    .pipe(purgecss({ content: ["*.html"] }))
+    .pipe(dest("css"));
+}
+
+function watchSass() {
+  watch(["sass/**/*.scss", "*.html"], buildStyles);
+}
+
+exports.default = series(buildStyles, watchSass);
---
+++ main.js
@@ -0,0 +1,2 @@
+import "./css/modern-reset.css";
+import "./css/index.css";
